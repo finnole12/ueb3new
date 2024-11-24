0efdffc3b4e3398baab04108f515c1dcd9cf4300
finnole12 <38003867+finnole12@users.noreply.github.com>
2024-11-24 20:26:17 +0100

    ueb4 complete

--- Client/src/main/java/ueb4/ClientMain.java
+++ Client/src/main/java/ueb4/ClientMain.java
@@ -12,6 +12,37 @@ public class ClientMain {
 
         gRPCServer.GrpcServer.RPC_Response getSize_response =
                 serviceStub.getSize(GrpcServer.GetSize_Request.newBuilder().build());
-        System.out.println(getSize_response.getResponse());
+        System.out.println("Size: " + getSize_response.getResponse());
+
+        serviceStub.addRecord(
+                GrpcServer.AddRecord_Request.newBuilder().setRecord("Appen").setIndex(4101).build()
+        );
+        serviceStub.addRecord(
+                GrpcServer.AddRecord_Request.newBuilder().setRecord("Ahrensburg").setIndex(4102).build()
+        );
+        serviceStub.addRecord(
+                GrpcServer.AddRecord_Request.newBuilder().setRecord("Wedel").setIndex(4103).build()
+        );
+        serviceStub.addRecord(
+                GrpcServer.AddRecord_Request.newBuilder().setRecord("Aumühle").setIndex(4104).build()
+        );
+        serviceStub.addRecord(
+                GrpcServer.AddRecord_Request.newBuilder().setRecord("Seevetal").setIndex(4105).build()
+        );
+        serviceStub.addRecord(
+                GrpcServer.AddRecord_Request.newBuilder().setRecord("Quickborn").setIndex(4106).build()
+        );
+
+        gRPCServer.GrpcServer.RPC_Response getRecord_response =
+                serviceStub.getRecord(GrpcServer.GetRecord_Request.newBuilder().setIndex(4103).build());
+        System.out.println("Record at 4103: " + getRecord_response.getResponse());
+
+        gRPCServer.GrpcServer.RPC_Response getRecord_response2 =
+                serviceStub.getRecord(GrpcServer.GetRecord_Request.newBuilder().setIndex(4103).build());
+        System.out.println("Record at 4107: " + getRecord_response2.getResponse());
+
+        gRPCServer.GrpcServer.RPC_Response getSize_response2 =
+                serviceStub.getSize(GrpcServer.GetSize_Request.newBuilder().build());
+        System.out.println("Size: " + getSize_response2.getResponse());
     }
 }
--- Server/src/main/java/ueb4/ServerImpl.java
+++ Server/src/main/java/ueb4/ServerImpl.java
@@ -1,12 +1,51 @@
 package ueb4;
 
 
+import gRPCServer.GrpcServer;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
+
 public class ServerImpl extends gRPCServer.ServiceGrpc.ServiceImplBase {
+
+    ArrayList<String> arrayList = new ArrayList<>();
+
+    public ServerImpl() {
+        initilizeDB();
+    }
 
     @Override
     public void getSize(gRPCServer.GrpcServer.GetSize_Request request,
                         io.grpc.stub.StreamObserver<gRPCServer.GrpcServer.RPC_Response> responseObserver) {
-        responseObserver.onNext(gRPCServer.GrpcServer.RPC_Response.newBuilder().setResponse("testresponse").build());
+        responseObserver.onNext(gRPCServer.GrpcServer.RPC_Response.newBuilder().setResponse("" + arrayList.size()).build());
         responseObserver.onCompleted();
+    }
+
+    @Override
+    public void addRecord(gRPCServer.GrpcServer.AddRecord_Request request,
+                          io.grpc.stub.StreamObserver<gRPCServer.GrpcServer.RPC_Response> responseObserver) {
+
+        arrayList.add( request.getIndex(), request.getRecord());
+
+        responseObserver.onNext(gRPCServer.GrpcServer.RPC_Response.newBuilder().setResponse("record added").build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getRecord(gRPCServer.GrpcServer.GetRecord_Request request,
+                          io.grpc.stub.StreamObserver<gRPCServer.GrpcServer.RPC_Response> responseObserver) {
+
+        String record = arrayList.get(request.getIndex());
+
+        responseObserver.onNext(GrpcServer.RPC_Response.newBuilder().setResponse(record).build());
+        responseObserver.onCompleted();
+    }
+
+    private void initilizeDB() {
+        for (int i = 0; i < 5000; i++) {
+            arrayList.add(i, "Random entry: " + new Random().nextInt());
+        }
     }
 }
